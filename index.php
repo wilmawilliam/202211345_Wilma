6b0b6dfad03c4dff82da0c47cb7485984eee1a7a
wilmawilliam <143744300+wilmawilliam@users.noreply.github.com>
2026-03-05 12:37:28 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wilma,Oplano PHP</title>
+</head>
+<body>
+    <h1>Wilma,Oplano PHP</h1>
+    
+    <pre>
+     oooo
+    o    o
+    o    o
+    o    o
+     oooo
+    </pre>
+    
+    <p>SHA256 hash of my name:</p>
+    <pre>
+    <?php
+    print hash('sha256', 'Wilma,Oplano');
+    ?>
+    </pre>
+
+
+    <h1>Wilma,Oplano</h1>
+
+    <pre>
+        
+        mOOOO
+        m O m
+        m O m
+        m O m
+
+    </pre>
+
+     <p>SHA256 hash of my name:</p>
+    <pre>
+    <?php
+    print '9c4868091505996f0994b4c787766d4615922c1d2a34031931b16328bba8bd89';
+    ?>
+</body>
+</html>
